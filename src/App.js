593896c310a351ef1168f2e24927a1c229addfd3
Priyanshu Agarwal <priyanshu.agarwal@nowfloats.com>
2018-08-13 21:24:00 +0530

    Basic architecture

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CompSvg from './animate_svg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <div>
+            <div class="sidebar">
+              <ul>
+                <li><i class="fas fa-home"></i></li>
+                <i class="fas fa-user"></i>
+                <i class="fas fa-asterisk"></i>
+              </ul>
+            </div>
+          </div>
+            {/* <CompSvg></CompSvg> */}
         </p>
       </div>
     );
