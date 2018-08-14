b3e4703ff5a279dfe0b1bfc85990c0998075869b
Priyanshu Agarwal <priyanshu.agarwal@nowfloats.com>
2018-08-14 16:58:53 +0530

    add arrow and use flex for container

--- src/home.js
+++ src/home.js
@@ -6,20 +6,30 @@ class Home extends Component {
     render() {
       return (
         <div>
-            <div className="home-text">
-                {/* <div className="row">
-                    <h1>Hi,</h1>
-                    <h2>I am Priyanshu</h2>
-                    <h2>full stack developer</h2>
-                </div> */}
-                <div className="bttn-wrapper row">
-                    <div className= "btn">
-                        {/* Explore more */}
+            <div class='home-container'>
+                <div className="">
+                    {/* <div className="row">
+                        <h1>Hi,</h1>
+                        <h2>I am Priyanshu</h2>
+                        <h2>full stack developer</h2>
+                    </div> */}
+                        <div className="row">
+                        <h1>lm,</h1>
+                        <h2>l hs awsblrdkw</h2>
+                        <h2>qwz lkwaq sqwcpolvr</h2>
+                    </div>
+                    <div className="bttn-wrapper row">
+                        <div className= "btn">
+                            erolpxE erom
+                        </div>
                     </div>
                 </div>
-            </div> 
-            <div className="svg-container">
-            {/* <CompSvg /> */}
+                <div className="svg-container">
+                    <CompSvg />
+                </div>
+            </div>
+            <div>
+                <i id="moveDown" class="fa fa-chevron-down fa-3x bounce"></i>
             </div>
         </div>
       );
