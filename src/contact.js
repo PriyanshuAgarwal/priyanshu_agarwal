43e9cca1648253d5a3af7bdc025f1f24760d45c1
PriyanshuAgarwal <agarwal.iet888@gmail.com>
2018-09-12 15:29:23 +0530

    fix css

--- src/contact.js
+++ src/contact.js
@@ -24,7 +24,15 @@ class Contact extends Component {
                     </form>
 
                 </div>
-                <div className='contact-social'>sadasd</div>
+                <div className='contact-social'>
+                    <div clssName='contact-social-media-icon-ctn'>
+                       <div class="img-ctn social-media-icon">
+                       <a href="" target='_top'>
+                            <img src={require('./images/email-color.svg')} alt='email' title ='email'/>
+                       </a>
+                       </div> 
+                    </div>
+                </div>
             </div>
         </div>
     </div>
