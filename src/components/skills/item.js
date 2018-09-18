02c1b4455cd83555fa63078ac4a5827bbe5688e3
PriyanshuAgarwal <agarwal.iet888@gmail.com>
2018-09-18 09:18:00 +0530

    Fixing ui changes on skills

--- src/components/skills/item.js
+++ src/components/skills/item.js
@@ -5,37 +5,17 @@ import PropTypes from 'prop-types';
 class SkillItem extends Component {
   constructor(props) {
     super(props);
-    // this.enableProgressBar = this.enableProgressBar.bind(this);
-    // this.disableProgressBar = this.disableProgressBar.bind(this);
   }
 
-  // enableProgressBar(event) {
-  //   const { percentage } = this.props;
-  //   console.log("Inside enable progressbar");
-  //   console.log(event.target);
-  //   console.log(event.target.childNodes);
-  //   event.target.childNodes[0].style.width = `${percentage}%`;
-  //   event.target.childNodes[1].style.width = '0px';
-  // //   document.getElementsByClassName("skill-progress-bar")[key].style.width = `${percentage}%`;
-  // //   document.getElementsByClassName("skill-progress-text")[key].style.width = "0px";
-  // }
-
-
-  // disableProgressBar(event){
-  //   const { key } = this.props;
-  //   event.target.childNodes[0].style.width = `0px`;
-  //   event.target.childNodes[1].style.width = '100px';
-  //   // document.getElementsByClassName("skill-progress-bar")[key].style.width = "0px";
-  //   // document.getElementByClassName("skill-progress-text").style.width = "0px;
-  // }
 
   render() {
+    const skillValue = this.props.percentage/10;
     return (
       <div className="skill-progress-item">
-        <div className="skill-progress-bar">
-          {this.props.percentage/10} / 10
+        <div className={`skill-progress-bar percentage-${skillValue}`}>
+          <span className='skill-progress-value'>{ `${skillValue}/ 10` } </span>
         </div>
-        <div className="skill-progress-text">{this.props.skillName}</div>
+         <div className="skill-progress-text">{this.props.skillName}</div>
       </div>
     );
   }
@@ -43,7 +23,7 @@ class SkillItem extends Component {
 
 SkillItem.propTypes = {
     skillName: PropTypes.string,
-    percentage: PropTypes.number,
+    percentage: PropTypes.string,
     key: PropTypes.number
 
 }
